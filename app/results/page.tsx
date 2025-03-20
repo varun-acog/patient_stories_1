bfdef4902a7ab291356b5e3f7e8d8ecdfe52a9e7
varun-acog <tvarun@aganitha.ai>
2025-03-20 06:46:07 +0000

    Updated analysis

--- app/results/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ResultsDisplay } from "@/components/results-display";
-
-export default function ResultsPage() {
-  return (
-    <main className="container mx-auto py-1">
-      <ResultsDisplay />
-    </main>
-  );
-}
